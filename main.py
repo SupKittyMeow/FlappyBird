61ce42589bd67cd92965f5cda94b177862dd637d
Max PC <supkittymeow@gmail.com>
2024-01-21 16:13:21 -0800

    adds better deco stuff and reformats width and height of screen

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
  
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720)) 
+screen = pygame.display.set_mode((432, 768)) 
 clock = pygame.time.Clock()
 running = True
 dt = 0
@@ -12,14 +12,18 @@ dt = 0
 player = pygame.image.load('Images/Bird/0.png')
 player = pygame.transform.scale(player, (85, 60))
 
+playerAnimation = 0
+shouldChangeAnimation = 0
+
 velocity = 0
 gravity = 0.5
 jumpHeight = 10
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 hasFlapped = False
 
-
-
+# background setup
+background = pygame.image.load('Images/Deco/BGDay.png')
+background = pygame.transform.scale(background, (432, 768))
 
 while running:
     # poll for events
@@ -29,7 +33,7 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.blit(background, (0, 0))
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
         if hasFlapped == False:
@@ -40,8 +44,23 @@ while running:
         hasFlapped = False
 
     player_pos.y += velocity
-    screen.blit(pygame.transform.rotate(player, -velocity * 5), (100, player_pos.y))
+    screen.blit(pygame.transform.rotate(player, -velocity * 5), (50, player_pos.y))
     velocity += gravity
+
+    shouldChangeAnimation += 1
+
+    if shouldChangeAnimation == 10:
+        shouldChangeAnimation = 0
+
+        # sets to next image
+        if playerAnimation == 2:
+            playerAnimation = 0
+        else:
+            playerAnimation += 1
+
+    player = pygame.image.load('Images/Bird/' + str(playerAnimation) + '.png')
+    player = pygame.transform.scale(player, (85, 60))
+    dt2 = clock.tick(10) / 1000
 
     # flip() the display to put your work on screen
     pygame.display.flip()
